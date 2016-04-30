fc736ae2fad00f307fb1c8d9fbed7af7494fcb8a
Ravaniss <senechal.max06@gmail.com>
2016-04-30 21:48:04 +0200

    Update can_fill_cell function for have correctly row, column and square

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,5 @@
 import os
 
-value_to_input = "1"
-
 
 def show_sudoku(sudoku):
 	for i in sudoku:
@@ -29,31 +27,34 @@ def grid_index(grid, value):
 	return -1, -1
 
 
-def can_fill_cell(sudoku, coords):
-	row = sudoku[0]
+def can_fill_cell(sudoku, coords, candidate):
+	row = sudoku[coords[0]]
 	for cell in row:
-		if cell == value_to_input:
+		if cell == candidate:
 			return False
 
-	column = [row[0] for row in sudoku]
+	column = [row[coords[1]] for row in sudoku]
 	for cell in column:
-		if cell == value_to_input:
+		if cell == candidate:
 			return False
 
-	square = [sudoku[x][y] for x in range(3) for y in range(3)]
+	# compute position of (upper left corner of) square containing cell at coords `coords`
+	square_i, square_j = (coords[0]//3)*3, (coords[1]//3)*3
+	# recover all cells in square
+	square = [sudoku[square_i + x][square_j + y] for x in range(3) for y in range(3)]
 	for cell in square:
-		if cell == value_to_input:
+		if cell == candidate:
 			return False
 
 	return True
 
 
-def solve_next_unsolved(sudoku):
-	coords = (2,2)
-	if can_fill_cell(sudoku, coords):
-		sudoku[coords[0]][coords[1]] = value_to_input
+def solve_next_unsolved(sudoku, candidate):
+	coords = (3,0)
+	if can_fill_cell(sudoku, coords, 2):
+		sudoku[coords[0]][coords[1]] = candidate
 		show_sudoku(sudoku)
 
 
-solve_next_unsolved(sudoku)
+solve_next_unsolved(sudoku, "2")
 os.system("pause")
